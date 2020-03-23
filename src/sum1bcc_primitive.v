51e062df3a34709188ab40a1ff4c119816014b30
johanCas320 <61796945+johanCas320@users.noreply.github.com>
2020-03-23 14:40:01 -0500

    Update sum1bcc_primitive.v

--- src/sum1bcc_primitive.v
+++ src/sum1bcc_primitive.v
@@ -1,16 +1,16 @@
 module sum1bcc_primitive (A, B, Ci,Cout,S);
-
+//se declaran 3 entradas y dos salidas
   input  A;
   input  B;
   input  Ci;
   output Cout;
   output S;
 
-
+//se declaran los cables que conectan las entradas
   wire a_ab;
   wire x_ab;
   wire cout_t;
-
+//se declaran las compuertas logicas que realizan la operacion en las entradas del cable
   and(a_ab,A,B);
   xor(x_ab,A,B);
 
@@ -19,4 +19,4 @@ module sum1bcc_primitive (A, B, Ci,Cout,S);
 
   or (Cout,cout_t,a_ab);
 endmodule
-
+//la diferencia con (sum1bcc.v) es que este no hace uso de memorias por lo que depende unicamente de operadores logicos y emtradas
